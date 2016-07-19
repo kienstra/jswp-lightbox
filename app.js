13aa5a162bb0cae2aeabd1192d497894d0d83d0e
Ryan Kienstra <ryan.kienstra@xwp.co>
2016-07-19 14:57:33 -0400

    Wrap app.js in a closure, to protect namespace. And remove extra function call.

--- app.js
+++ app.js
@@ -1,52 +1,54 @@
-/********************************
- * Simple Lightbox
- * 1.4.1.8
- *
- *******************************/
+( function() {
 
-var body = document.querySelector( 'body' ),
-	lightboxDemo = document.getElementById( 'lightbox-demo' ),
-	gallery = document.querySelectorAll( '#lightbox-demo .gallery' )[0],
-	overlay = document.createElement( 'div' ),
-	overlayCloseLink = document.createElement( 'a' ),
-	overlayCloseText = document.createTextNode( 'X' ),
-	displayOverlay,
-	openLightbox,
-	closeLightBox,
-	addImageToOverlay;
+	/********************************
+	 * Simple Lightbox
+	 * 1.4.1.8
+	 *
+	 *******************************/
 
-closeLightBox = function closeLightBox( e ) {
+	var body = document.querySelector( 'body' ),
+		gallery = document.querySelectorAll( '#lightbox-demo .gallery' )[0],
+		overlay = document.createElement( 'div' ),
+		overlayCloseLink = document.createElement( 'a' ),
+		overlayCloseText = document.createTextNode( 'X' ),
+		closeLightBox,
+		displayOverlay,
+		addImageToOverlay,
+		openLightbox;
 
-	e.preventDefault();
-	overlayCloseLink.removeEventListener( 'click', closeLightBox, false );
-	overlay.querySelector( 'img' ).remove();
-	overlay.remove();
+	closeLightBox = function closeLightBox( e ) {
 
-};
-
-displayOverlay = function displayOverlay() {
-
-	overlay.setAttribute( 'id', 'overlay'  );
-	overlayCloseLink.appendChild( overlayCloseText );
-	overlayCloseLink.setAttribute( 'href', '#' );
-	overlayCloseLink.classList.add( 'close' );
-	overlayCloseLink.addEventListener( 'click', closeLightBox, false );
-
-	overlay.appendChild( overlayCloseLink );
-	body.appendChild( overlay );
-
-};
-
-addImageToOverlay = function addImageToOverlay( img ) {
-	overlay.appendChild( img )
-};
-
-openLightbox = function openLightbox( e ) {
-	if ( e.target && 'IMG' === e.target.nodeName ) {
 		e.preventDefault();
-		displayOverlay();
-		addImageToOverlay( e.target.cloneNode() );
-	}
-};
+		overlayCloseLink.removeEventListener( 'click', closeLightBox, false );
+		overlay.querySelector( 'img' ).remove();
+		overlay.remove();
 
-gallery.addEventListener( 'click', openLightbox );
+	};
+
+	displayOverlay = function displayOverlay() {
+
+		overlay.setAttribute( 'id', 'overlay'  );
+		overlayCloseLink.appendChild( overlayCloseText );
+		overlayCloseLink.setAttribute( 'href', '#' );
+		overlayCloseLink.classList.add( 'close' );
+		overlayCloseLink.addEventListener( 'click', closeLightBox, false );
+		overlay.appendChild( overlayCloseLink );
+		body.appendChild( overlay );
+
+	};
+
+	addImageToOverlay = function addImageToOverlay( img ) {
+		overlay.appendChild( img )
+	};
+
+	openLightbox = function openLightbox( e ) {
+		if ( e.target && 'IMG' === e.target.nodeName ) {
+			e.preventDefault();
+			displayOverlay();
+			addImageToOverlay( e.target.cloneNode() );
+		}
+	};
+
+	gallery.addEventListener( 'click', openLightbox );
+
+}() );
