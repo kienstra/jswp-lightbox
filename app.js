35f511d4257564b70f122070775950bef3fb0603
Ryan Kienstra <ryan.kienstra@xwp.co>
2016-07-19 10:33:09 -0400

    Uncomment lighbox functionality in app.js, convert spaces to tabs.

--- app.js
+++ app.js
@@ -582,68 +582,52 @@
  *
  *******************************/
 
-// var body = document.querySelector( 'body' ),
-//     lightboxDemo = document.getElementById( 'lightbox-demo' ),
-//     lightboxLinks = document.querySelectorAll( 'a.lightbox' ),
-//     wapuuLink = lightboxLinks[0],
-//     overlay = document.createElement( 'div' ),
-//     overlayCloseLink = document.createElement( 'a' ),
-//     overlayCloseText = document.createTextNode( 'X' ),
-//     displayOverlay,
-//     openLightbox,
-//     closeLightBox,
-//     addImageToOverlay;
-//
-// closeLightBox = function closeLightBox( e ) {
-//
-//   e.preventDefault();
-//   overlayCloseLink.removeEventListener( 'click', closeLightBox, false );
-//   overlay.querySelector( 'img' ).remove();
-//   overlay.remove();
-//
-// };
-//
-// displayOverlay = function displayOverlay() {
-//
-//   overlay.setAttribute( 'id', 'overlay'  );
-//   overlayCloseLink.appendChild( overlayCloseText );
-//   overlayCloseLink.setAttribute( 'href', '#' );
-//   overlayCloseLink.classList.add( 'close' );
-//   overlayCloseLink.addEventListener( 'click', closeLightBox, false );
-//
-//   overlay.appendChild( overlayCloseLink );
-//   body.appendChild( overlay );
-//   //console.log( 'here' );
-//
-// };
-//
-// addImageToOverlay = function addImageToOverlay( img ) {
-//
-//   overlay.appendChild( img )
-//
-// }
-//
-// openLightbox = function openLightbox( e ) {
-//
-//   e.preventDefault();
-//   displayOverlay();
-//   addImageToOverlay( e.target.cloneNode() );
-//
-// };
-//
-//
-//
-// wapuuLink.addEventListener( 'click', openLightbox );
-//
-// lightboxDemo.style.display = 'block';
+var body = document.querySelector( 'body' ),
+    lightboxDemo = document.getElementById( 'lightbox-demo' ),
+    lightboxLinks = document.querySelectorAll( 'a.lightbox' ),
+    wapuuLink = lightboxLinks[0],
+    overlay = document.createElement( 'div' ),
+    overlayCloseLink = document.createElement( 'a' ),
+    overlayCloseText = document.createTextNode( 'X' ),
+    displayOverlay,
+    openLightbox,
+    closeLightBox,
+    addImageToOverlay;
 
+closeLightBox = function closeLightBox( e ) {
 
+	e.preventDefault();
+	overlayCloseLink.removeEventListener( 'click', closeLightBox, false );
+	overlay.querySelector( 'img' ).remove();
+	overlay.remove();
 
+};
 
+displayOverlay = function displayOverlay() {
 
+	overlay.setAttribute( 'id', 'overlay'  );
+	overlayCloseLink.appendChild( overlayCloseText );
+	overlayCloseLink.setAttribute( 'href', '#' );
+	overlayCloseLink.classList.add( 'close' );
+	overlayCloseLink.addEventListener( 'click', closeLightBox, false );
 
+	overlay.appendChild( overlayCloseLink );
+	body.appendChild( overlay );
 
+};
 
+addImageToOverlay = function addImageToOverlay( img ) {
+	overlay.appendChild( img )
+};
 
+openLightbox = function openLightbox( e ) {
 
-// End 1.4.1
+	e.preventDefault();
+	displayOverlay();
+	addImageToOverlay( e.target.cloneNode() );
+
+};
+
+wapuuLink.addEventListener( 'click', openLightbox );
+
+lightboxDemo.style.display = 'block';
